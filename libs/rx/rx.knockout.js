6bab06ab1a35c603b519baff84797683e3695ac3
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-05-25 17:53:39 +0200

    as matt pointed out, we shouldn't rely on __proto__ to be there

--- libs/rx/rx.knockout.js
+++ libs/rx/rx.knockout.js
@@ -1,33 +1,42 @@
-(function(){
+(function () {
     var global = this,
-	root = (typeof ProvideCustomRxRootObject == "undefined") ? global.Rx : ProvideCustomRxRootObject();
-    
+        root = (typeof ProvideCustomRxRootObject == "undefined") ? global.Rx : ProvideCustomRxRootObject();
+
     var observable = root.Observable;
     var asyncSubject = root.AsyncSubject;
     var observableCreate = observable.Create;
     var disposableEmpty = root.Disposable.Empty;
 
-    ko.observable.__proto__.toObservable = function(){
-        var koObservable = this;
-        return observableCreate(function(observer){
-			var disposable = koObservable.subscribe(function(data){
-				observer.OnNext(data);
-			});
-            return function(){
-                disposable.dispose();
-            };
+    if (!Object.getPrototypeOf) {
+        Object.getPrototypeOf = function getPrototypeOf(object) {
+            return object.__proto__ || object.constructor.prototype;
+            // or undefined if not available in this engine
+        };
+    }
+
+    if (typeof Object.getPrototypeOf !== 'undefined') {
+        Object.getPrototypeOf(ko.observable).toObservable = function () {
+            var koObservable = this;
+            return observableCreate(function (observer) {
+                var disposable = koObservable.subscribe(function (data) {
+                    observer.OnNext(data);
+                });
+                return function () {
+                    disposable.dispose();
+                };
+            });
+        };
+    }
+
+    root.Observable.prototype.toKoObservable = function () {
+        var koObservable = ko.observable();
+        var disposable = this.Subscribe(function (data) {
+            koObservable(data);
         });
+        koObservable.dispose = function () {
+            disposable.Dispose();
+        };
+        return koObservable;
     };
-	
-	root.Observable.prototype.toKoObservable = function(){
-		var koObservable = ko.observable();
-		var disposable = this.Subscribe(function(data){
-			koObservable(data);
-		});
-		koObservable.dispose = function(){
-			disposable.Dispose();
-		};
-		return koObservable;		
-	};
-	
+
 })();
